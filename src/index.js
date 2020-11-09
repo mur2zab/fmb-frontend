61b0e5111a196c0784e7a30f09a9929ba5b930a7
huzefa <huzefasr9@gmail.com>
2020-11-09 20:27:25 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ import { ApolloProvider } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "https://apollo-graphql-mongoose.herokuapp.com/",
+  // uri: "https://apollo-graphql-mongoose.herokuapp.com/",
   // uri: "http://localhost:4000/",
+  uri: "http://68.183.247.164:4000/",
 });
 
 const authLink = setContext((_, { headers }) => {
